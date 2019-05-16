c1cc2b58686901a99900fdbb4db49e3375327182
doradoe1 <doradoe1@outlook.com>
2019-05-16 16:38:49 -0500

    fixed node

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require ("express");
 const app = express();
-const hostname = 'localhost';
 const port = 8080;
 const path = require ('path');
 
-
-app.listen(port, hostname, () => {
-  console.log(`Server OUR Chicken up and running at localhost:8080`);
-});
-
-app.use(express.static('C:/revature/revature-p2/'));
+app.use(express.static(__dirname));
 
 app.get('/', (req,res) =>{
-  res.sendFile(`C:/revature/revature-p2/`)
+  res.sendFile(path.join(__dirname + `./index.html`))
 });
+
+app.listen(port, () => 
+  console.log(`Server OUR Chicken up and running at ${port}`)
+);
+
