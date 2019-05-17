4df975211e8d25fe544449871da15c9450b35699
doradoe1 <doradoe1@outlook.com>
2019-05-17 10:57:18 -0500

    azure cli and api

--- app.js
+++ app.js
@@ -13,3 +13,12 @@ app.listen(port, () =>
   console.log(`Server OUR Chicken up and running at ${port}`)
 );
 
+//const http = require('http');
+//const fs = require('fs');
+//http.createServer(function (req, res) {
+ // fs.readFile('index.html', function(err, data) {
+  //  res.writeHead(200, {'Content-Type': 'text/html'});
+   // res.write(data);
+    //res.end();
+  //});
+//}).listen(8080);
